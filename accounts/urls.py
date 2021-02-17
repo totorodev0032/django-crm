ff9f3b36e12583dc98675fc05100777d11de8814
nagendra0032 <nagendrakumar0032@gmail.com>
2021-02-17 23:26:29 +0530

    dynamic url added

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/<str:primaryKey>', views.customer),
+    path('', views.home, name="home"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:primaryKey>', views.customer, name="customer"),
 ]
