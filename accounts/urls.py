47032edb6b895b5359f28eaa3ade3eb82c7f61d3
nagendra0032 <nagendrakumar0032@gmail.com>
2021-02-26 18:19:20 +0530

    authentication done

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
          views.updateOrder, name="update_order"),
 
     path('delete_order/<str:primaryKey>',
-         views.deleteOrder, name="delete_order")
+         views.deleteOrder, name="delete_order"),
+
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout")
 ]
--- crm/settings.py
+++ crm/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysqldemo',
+        'USER': 'root',
+        'PASSWORD': 'Nagendra12@',
+        'PORT': 3306,
+        'HOST': '127.0.0.1'
     }
 }
 
